397ea344c5a9837cd7403ccb208d31a319d511a2
sfespinosa <scottjames.espinosa@gmail.com>
2020-07-22 15:26:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+const map = (array, fn) => {
+    let newArray = []
+    for (let element of array) {
+        newArray.push(fn(element))
+    }
+    return newArray
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
