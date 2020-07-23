0168de3a7185bbc441dfecdfd80afc4b9721e498
iona-b <ionabrabender@gmail.com>
2020-07-23 13:57:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+const map = (sourceArray, callbackFunction) => {
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        let element = sourceArray[i]
+        newArray.push(callbackFunction(element))
+    }
+    return newArray
+}
+
+const reduce = (sourceArray, callbackFunction, initialValue) => {
+    if (initialValue === undefined) {
+        initialValue = sourceArray[0]
+        sourceArray = sourceArray.slice(1)
+    }
+    for (let i = 0; i < sourceArray.length; i++) {
+        initialValue = callbackFunction(sourceArray[i], initialValue)
+    }
+    return initialValue
+}
